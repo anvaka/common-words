0d3a49f40be288db828b8cca870c3b40ac9fdb93
Andriy Kashcha <anvaka@gmail.com>
2016-12-19 08:51:53 -0800

    Added stylus loaded
    Chopping away one step at a time.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <h3>Common Language</h3>
+    <h4>
+      Visualization of most common words in code
+    </h4>
+    <blockquote>
+      <p>I querried 1.9TB of source code to build this website!</p>
+      <footer>anvaka - author of `Common Language`</footer>
+    </blockquote>
   </div>
 </template>
 
@@ -16,13 +22,27 @@ export default {
 };
 </script>
 
-<style>
+<style lang="styl">
+ * {
+    box-sizing: border-box;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    font-family: Abel, sans-serif;
+  }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h3, h4 {
+  text-align: center;
+}
+blockquote {
+  box-shadow: 0 0 6px rgba(0,0,0,0.5);
+  padding: .75em .5em .75em 1em;
+  max-width: 400px;
+  border-left: 0.5em solid #DDD;
 }
 </style>
